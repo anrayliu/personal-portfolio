c7e7aa0ca5892ea3e87439d2e4772730e3f8f03d
Anray Liu <anrayliu@gmail.com>
2025-08-15 23:49:56 -0400

    chore: update build files

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\joewo\AppData\Local\Temp\tmpx6rqc7rg.js
+// include: C:\Users\joewo\AppData\Local\Temp\tmpy5u6wio4.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -208,21 +208,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: C:\Users\joewo\AppData\Local\Temp\tmpx6rqc7rg.js
-// include: C:\Users\joewo\AppData\Local\Temp\tmpy01m6098.js
+// end include: C:\Users\joewo\AppData\Local\Temp\tmpy5u6wio4.js
+// include: C:\Users\joewo\AppData\Local\Temp\tmpjyx0pf3n.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\joewo\AppData\Local\Temp\tmpy01m6098.js
-// include: C:\Users\joewo\AppData\Local\Temp\tmp4juxfid1.js
+  // end include: C:\Users\joewo\AppData\Local\Temp\tmpjyx0pf3n.js
+// include: C:\Users\joewo\AppData\Local\Temp\tmpuif1_ho9.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\joewo\AppData\Local\Temp\tmp4juxfid1.js
+  // end include: C:\Users\joewo\AppData\Local\Temp\tmpuif1_ho9.js
 
 
 var arguments_ = [];
