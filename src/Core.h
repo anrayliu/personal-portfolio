de33b7a8f5557a7effc10bb07705f5a80249c759
Anray Liu <anrayliu@gmail.com>
2025-08-19 21:14:24 -0400

    refactor: reduce public members in core

--- src/Core.h
+++ src/Core.h
@@ -60,18 +60,13 @@ class Core {
 
     ~Core();
 
-public:
     // delete copy constructor and copy assignment operator
     Core(Core &core) = delete;
 
     void operator=(const Core &) = delete;
 
-    static Core* get_instance();
-
     int mousex{};
     int mousey{};
-    bool click{};
-    bool middle_click{};
 
     bool dragging;
 
@@ -100,24 +95,32 @@ public:
     SDL_Point empty_view_dimensions{};
     SDL_Point project_name_dimensions{};
 
-    Timer timer;
-
-    bool quit;
 
     std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> win;
     std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer;
     std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)> font;
 
-    static void init_sdl();
+public:
+    bool click{};
+    bool middle_click{};
+    Timer timer;
 
-    static void quit_sdl();
-    static std::shared_ptr<SDL_Texture> load_texture(SDL_Renderer* renderer, const string &path, int w, int h);
-    static std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> load_text(SDL_Renderer* renderer, TTF_Font* font,
-        const string &text);
+    bool quit;
+
+    void update();
 
     void init();
 
-    void update();
+    static void init_sdl();
+
+    static void quit_sdl();
+
+    static Core *get_instance();
+
+    static std::shared_ptr<SDL_Texture> load_texture(SDL_Renderer *renderer, const string &path, int w, int h);
+
+    static std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> load_text(SDL_Renderer *renderer, TTF_Font *font,
+        const string &text);
 };
 
 #endif
